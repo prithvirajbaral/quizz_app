c9dd4ce1d60c2589b31fc6e11360cd5c7de74007
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-27 13:00:07 +0530

    this is the custom widget which is created to return elevatedButton separatly

---
+++ lib/answers_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnswersButton extends StatelessWidget {
+  // const AnswersButton(this.answerText, this.onTap, {super.key});
+  const AnswersButton(
+      {required this.answerText, required this.onTap, super.key});
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),//for horizontal and verticall padding differently
+        backgroundColor: const Color.fromARGB(255, 50, 1, 102),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
