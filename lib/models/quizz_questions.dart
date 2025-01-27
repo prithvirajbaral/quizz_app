4ece0e37182e9c7ee0acbddb0b0e40db92a5d983
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-27 14:56:36 +0530

    added a new function getShuffledList for shuffled the answer list.

--- lib/models/quizz_questions.dart
+++ lib/models/quizz_questions.dart
@@ -3,4 +3,10 @@ class QuizzQuestions {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledList() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
