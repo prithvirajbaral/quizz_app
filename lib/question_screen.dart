c3e0ce3a4571d01357580730487ed8f738de347c
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-27 14:45:02 +0530

    add Container widget for margin. also add crossAxisAlignment: CrossAxisAlignment.stretch, // it will stretch out as wide as possible to fill the entire column width

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -17,20 +17,26 @@ class _QuestionScreen extends State<QuestionScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity, //to take as much space you can
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map((answer) {
-            return AnswersButton(answerText: answer, onTap: () {});
-          })
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment
+              .stretch, // it will stretch out as wide as possible to fill the entire column width
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map((answer) {
+              return AnswersButton(answerText: answer, onTap: () {});
+            })
+          ],
+        ),
       ),
     );
   }
