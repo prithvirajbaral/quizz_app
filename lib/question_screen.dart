1aeef98f2ccd2bc5c632067a1afe1d37d657e6d3
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-27 15:48:18 +0530

    add googleFont package to design the question text by cmd line "flutter pub add google-fonts"

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quizz_app/answers_button.dart';
 import 'package:quizz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -34,7 +35,10 @@ class _QuestionScreen extends State<QuestionScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(
